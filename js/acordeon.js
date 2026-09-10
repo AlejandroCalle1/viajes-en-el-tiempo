16ca61fd83642c770cda65a3b1be406311fd7f86
Juan Alejandro Calle <juan.calle.z@uniautonoma.edu.co>
2026-09-10 10:41:21 -0500

    correccion de algunos estilos de css

--- js/acordeon.js
+++ js/acordeon.js
@@ -1,8 +1,9 @@
-const botones = document.querySelectorAll('.boton-acordeon');
+const botones = document.querySelectorAll('.boton-acordeon');   // const variable que selecciona todos los elementos con la clase 'boton-acordeon' , document es representa la pagina html, querySelectorAll es un metodo que selecciona todos los elementos que coinciden con el selector especificado, en este caso '.boton-acordeon'
 
-botones.forEach(boton => {
-    boton.addEventListener('click', function() {
-        const respuesta = this.nextElementSibling;
-        respuesta.classList.toggle('mostrar');
+botones.forEach(boton => {   //significa "para cada uno". Es un método que le dice a JavaScript: "toma la caja botones (que tiene 7 elementos adentro) y repite el siguiente código una vez por cada uno de esos 7 botones".
+                //boton singular es el nombre que le damos a cada uno de los elementos que estamos recorriendo, es decir, cada uno de los 7 botones.
+    boton.addEventListener('click', function() { // agrega un evento de escucha a cada botón, que se activa cuando se hace clic en él. Cuando se hace clic, se ejecuta la función que sigue.
+        const respuesta = this.nextElementSibling; // this hace referencia al botón que fue clickeado, nextElementSibling selecciona el siguiente elemento osea respuesta 
+        respuesta.classList.toggle('mostrar');   // hace que aparezca 
     });
 });
